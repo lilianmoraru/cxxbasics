ea5018759c605ca791d501d525618b08466b6707
Lilian A. Moraru <lilian.moraru90@gmail.com>
2017-03-03 20:57:16 +0200

    Add an assert to make sure folders are writable

---
+++ helpers/Asserts.cmake
@@ -0,0 +1,18 @@
+## This module contains different helper asserts
+
+cmake_minimum_required(VERSION 3.8 FATAL_ERROR)
+
+function(CXXBASICS_ASSERT_WRITABLE_FOLDER  folder_path)
+  include(helpers/FnMktemp)
+
+  # Lets attempt to create a random file
+  set(CXXBASICS_TMP_FOLDER "${folder_path}")
+  mktemp()
+
+  if(NOT DEFINED mktemp_result)
+    cberror("Folder \"${folder_path}\" is not writable")
+  else()
+    # Removing the temporary file
+    file(REMOVE "${mktemp_result}")
+  endif()
+endfunction()
