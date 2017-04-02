0b4372b8a8195a6de5134a30427924387225a4f8
Lilian A. Moraru <lilian.moraru90@gmail.com>
2017-04-02 22:12:28 +0300

    Allow the user to extend CXXBasics

--- CXXBasics.cmake
+++ CXXBasics.cmake
@@ -13,3 +13,8 @@ include(accelerators/UseFasterLinkers)
 
 # Activate the compiler cache tool
 include(accelerators/UseCompilerCacheTool)
+
+# Allow the user to extend CXXBasics
+if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/../cxxbasics-extension.cmake")
+  include("${CMAKE_CURRENT_LIST_DIR}/../cxxbasics-extension.cmake")
+endif()
